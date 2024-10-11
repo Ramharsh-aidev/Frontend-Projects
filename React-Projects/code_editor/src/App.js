45cb5303021a814837359e8ea6a21d25a5c258f6
Ramharsh-aidev <ramharshdandekar@gmail.com>
2024-10-11 22:19:16 +0530

    Added Navbar

---
+++ React-Projects/code_editor/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const App = () => {
+    return (
+        <div className="App">
+            <Navbar/>
+        </div>
+    );
+};
+
+export default App;
---
+++ React-Projects/code_editor/src/components/Navbar.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import '../CSS_Files/Navbar.css';
+import GFGNavbarLogo from '../Images/css-logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+
+const Navbar = () => {
+    const [theme, setTheme] = useState('light');
+
+    const handleTheme = () => {
+        if (theme === 'light') {
+            document.documentElement.setAttribute('data-bs-theme', 'dark');
+            setTheme('dark');
+        } else {
+            document.documentElement.setAttribute('data-bs-theme', 'light');
+            setTheme('light');
+        }
+    };
+
+    return (
+        <nav className="navbar navbar-expand-lg">
+            <a className="navbar-brand" href="/">
+                <img src={GFGNavbarLogo} alt="Logo" />
+            </a>
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul className="navbar-nav me-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/events">About Us</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/Open App">Open App</a>
+                    </li>
+                </ul>
+                <form className="d-flex me-4">
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button className="btn btn-outline-success" type="submit">Search</button>
+                </form>
+                <div className="form-check form-switch">
+                    <input
+                        className="form-check-input"
+                        type="checkbox"
+                        id="themeSwitch"
+                        onChange={handleTheme}
+                    />
+                    <label className="form-check-label" htmlFor="themeSwitch">
+                        <FontAwesomeIcon icon={theme === 'light' ? faSun : faMoon} />
+                    </label>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
